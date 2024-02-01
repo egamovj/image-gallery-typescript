852e786ad73c48f3f8d91559c25f6cb0dcc804ea
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-02 00:21:31 +0500

    changed styles

--- src/components/Cards/ImageCard.tsx
+++ src/components/Cards/ImageCard.tsx
@@ -11,7 +11,7 @@ const ImageCard = ({id, slug, isSelected, onClick, className=""}: ImageCard) => 
     const {attributes, listeners, setNodeRef, transform, transition, isDragging, index} = useSortable({id: id})
   return (
     <div className={twMerge("relative rounded-lg overflow-hidden border border-gray-300 group z-0 aspect-square object-cover")}>
-        <div>
+        <div className={twMerge("flex items-center justify-center h-full", isSelected && "opacity-60")}>
             <img src={slug} alt="" className="block h-full w-full object-cover" />
         </div>
     </div>
