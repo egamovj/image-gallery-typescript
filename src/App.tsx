e26ffa085004d2c1c6d9fa2b13ce78a3f8a2a724
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-01 20:37:14 +0500

    added dnd sensors code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from '@dnd-kit/core'
 import './App.css'
+import { sortableKeyboardCoordinates } from '@dnd-kit/sortable'
 
 function App() {
 
+  // dnd code
+  const sensors = useSensors(
+      useSensor(PointerSensor),
+      useSensor(KeyboardSensor, {
+        coordinateGetter: sortableKeyboardCoordinates
+      }),
+      useSensor(TouchSensor)
+  );
+
+  const handleDragStart = () => {
+    
+  }
+
   return (
-    <>
-      <span className='text-red-600'>Hello TS</span>
-    </>
+    <div className='min-h-screen'>
+      <div className='container flex flex-col items-center'>
+        <div className='bg-white my-8 rounded-lg shadow max-w-5xl grid divide-y'>
+          <header className='text-2xl'>Showcase</header>
+          <DndContext 
+          sensors={sensors} 
+          collisionDetection={closestCenter}
+          onDragStart={handleDragStart}
+          >
+            <div>
+              Image Gallery
+            </div>
+          </DndContext>
+        </div>
+      </div>
+    </div>
   )
 }
 
