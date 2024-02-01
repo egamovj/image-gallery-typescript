ba68b6b19324d77de83767a7d5961e324cbab759
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-01 20:57:36 +0500

    dnd code ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from '@dnd-kit/core'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { DndContext, DragEndEvent, DragStartEvent, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from '@dnd-kit/core'
 import './App.css'
-import { sortableKeyboardCoordinates } from '@dnd-kit/sortable'
+import { arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable'
+import { useState } from 'react';
+import { ImageGallery } from './types/global.types';
+import { initialImageData } from './data';
 
 function App() {
+  const [galleryData, setGalleryData] = useState(initialImageData);
 
   // dnd code
+  const [activeItem, setActiveItem] = useState<ImageGallery | null>(null)
   const sensors = useSensors(
       useSensor(PointerSensor),
       useSensor(KeyboardSensor, {
@@ -13,9 +19,27 @@ function App() {
       useSensor(TouchSensor)
   );
 
-  const handleDragStart = () => {
-    
-  }
+  const handleDragStart = (event: DragStartEvent) => {
+    const {id} = event.active;
+    if(!id) return;
+    const currentItem = galleryData.find((item) => item.id === id);
+    setActiveItem(currentItem || null);
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    setActiveItem(null);
+    const {active, over} = event;
+    if(!over) {
+      return;
+    }
+    if(active.id !== over.id) {
+      setGalleryData((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+        return arrayMove(items, oldIndex, newIndex)
+      })
+    }
+  };
 
   return (
     <div className='min-h-screen'>
@@ -26,6 +50,7 @@ function App() {
           sensors={sensors} 
           collisionDetection={closestCenter}
           onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
           >
             <div>
               Image Gallery
