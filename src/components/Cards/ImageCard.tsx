1f3afcc57619eba3597f96831768cef26991d40c
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-02 09:07:53 +0500

    changed ImageCard style

--- src/components/Cards/ImageCard.tsx
+++ src/components/Cards/ImageCard.tsx
@@ -1,6 +1,7 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { ImageGallery } from "../../types/global.types"
 import { twMerge } from "tailwind-merge";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ImageCard extends ImageGallery {
     className?: string,
@@ -9,8 +10,21 @@ interface ImageCard extends ImageGallery {
 
 const ImageCard = ({id, slug, isSelected, onClick, className=""}: ImageCard) => {
     const {attributes, listeners, setNodeRef, transform, transition, isDragging, index} = useSortable({id: id})
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+      zIndex: isDragging ? "100" : "auto",
+      opacity: isDragging ? 0.3 : 1,
+      gridRow: index === 0 ? "span 2" : "span 1",
+      gridColumn: index === 0 ? "span 2" : "span 1",
+      transformOrigin: "0 0", 
+    }
   return (
-    <div className={twMerge("relative rounded-lg overflow-hidden border border-gray-300 group z-0 aspect-square object-cover")}>
+    <div 
+    ref={setNodeRef}
+    style={style}
+    className={twMerge("relative rounded-lg overflow-hidden border border-gray-300 group z-0 aspect-square object-cover", className)}>
+
         <div className={twMerge("flex items-center justify-center h-full", isSelected && "opacity-60")}>
             <img src={slug} alt="" className="block h-full w-full object-cover" />
         </div>
